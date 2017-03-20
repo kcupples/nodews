8bc88dd8ab1e467f6e03e602dd726364e6ae117c
Cupples <kcupples@acumensolutions.com>
2017-03-20 16:19:23 -0400

    Updated index.js again

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
-var pg = require('pg');
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
-app.get('/', function(request,response){
-	response.render('Hello');
+app.get('/', function (request, response) {
+   response.send("Hi There");
 });
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
