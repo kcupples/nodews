e47332c0faa8123eaf0171a972a174bee1560ab7
Cupples <kcupples@acumensolutions.com>
2017-03-20 16:13:04 -0400

    Added Static Content

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+var express = require('express');
 var pg = require('pg');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request,response){
+	response.render('pages/index.html');
+});
+
 
 app.get('/db', function(request, response)
 {
