a26cc81e4942fad8a9b449497b80b6a171603739
Cupples <kcupples@acumensolutions.com>
2017-03-20 17:23:32 -0400

    More Soap Stuff

--- index.js
+++ index.js
@@ -3,39 +3,6 @@ var app = express();
 var pg = require('pg');
 var soap = require('soap');
 
-var myService = {
-	MyService: {
-			MyPort: {
-				MyFunction: function(args){
-					return {
-						name: args.name
-					};
-				},
-				 // This is how to define an asynchronous function. 
-              MyAsyncFunction: function(args, callback) {
-                  // do some work 
-                  callback({
-                      name: args.name
-                  });
-              },
- 
-              // This is how to receive incoming headers 
-              HeadersAwareFunction: function(args, cb, headers) {
-                  return {
-                      name: headers.Token
-                  };
-              },
- 
-              // You can also inspect the original `req` 
-              reallyDetailedFunction: function(args, cb, headers, req) {
-                  console.log('SOAP `reallyDetailedFunction` request from ' + req.connection.remoteAddress);
-                  return {
-                      name: headers.Token
-                  };
-              }
-          }
-      }
-  };
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
@@ -64,8 +31,4 @@ app.get('/db', function(request, response){
 	});
 });
 
-app.listen(8001, function(){
-    //Note: /wsdl route will be handled by soap module 
-    //and all other routes & middleware will continue to work 
-    soap.listen(app, '/wsdl', service, xml);
-  });
+app.use('/soap', require('./lib/soaplistener.js')());
