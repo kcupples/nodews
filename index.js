3bec97764f3f92f715c2acf00a7133fe36ec9f11
Cupples <kcupples@acumensolutions.com>
2017-03-20 16:16:20 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request,response){
-	response.render('pages/index.html');
+	response.render('Hello');
 });
 
-
-app.get('/db', function(request, response)
-{
-	pg.connect(process.env.DATABASE_URL, function(err, client,done){
-		client.query('SELECT * FROM test_table', function(err,result){
-			done();
-			if (err){
-					console.error(err);
-					response.send("Error: " + err);
-			}
-			else{
-				response.render('pages/db', {results: result.rows});
-			}
-
-		});
-	});
-});
