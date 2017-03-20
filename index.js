ba1557a4b2282de9ded312f5714691541ac47c50
Cupples <kcupples@acumensolutions.com>
2017-03-20 16:28:43 -0400

    Added DB Stuff

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
 
@@ -9,4 +11,17 @@ app.get('/', function (request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/db', function(request, response){
+	pg.connect(process.env.DATABASE_URL, function(err, client, done){
+		done();
+		if (err){
+			console.error(err);
+			response.send("Error: " + err);
+		}
+		else{
+			response.render('pages/db', {results: result.rows})
+		}
+	});
 });
