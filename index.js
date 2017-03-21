504e99355522cf41d7b2052a513b118af2c33362
Cupples <kcupples@acumensolutions.com>
2017-03-21 00:25:38 -0400

    Updating SOAP stuff

--- index.js
+++ index.js
@@ -75,11 +75,11 @@ var MyService = {
 //app.listen(app.get('port'), function() {
 //  console.log('Node app is running on port', app.get('port'));
 //});
-
-//app.get('/db', function(request, response){
-	//pg.connect(process.env.DATABASE_URL, function(err, client, done){
-		//client.query('SELECT * from test_table', function(err, result)
-		//{
+*/
+app.get('/db', function(request, response){
+	pg.connect(process.env.DATABASE_URL, function(err, client, done){
+		client.query('SELECT * from test_table', function(err, result)
+		{
 			//done();
 			if (err){
 				console.error(err);
