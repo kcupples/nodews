2dd75e7a1e92827435bd905ed9ce10b97875af96
Cupples <kcupples@acumensolutions.com>
2017-03-21 08:10:19 -0400

    Updating SOAP stuff

--- index.js
+++ index.js
@@ -121,7 +121,29 @@ var MyService = {
 */
 
 
-//Send the WSDL to anyone requesting
-app.get('/wsdl', function (request, response) {
-   response.send(xml);
+
+//app.get('/', function (request, response) {
+   //response.send("Hi There");
+//});
+
+//app.listen(app.get('port'), function() {
+//  console.log('Node app is running on port', app.get('port'));
+//});
+
+app.get('/db', function(request, response){
+	pg.connect(process.env.DATABASE_URL, function(err, client, done){
+		client.query('SELECT * from test_table', function(err, result)
+		{
+			//done();
+			if (err){
+				console.error(err);
+				response.send("Error: " + err);
+			}
+			else {
+				response.send(result.rows);
+			}
+		});
+	});
 });
+
+//app.use('/soap', require('./lib/soaplistener.js')());*/
